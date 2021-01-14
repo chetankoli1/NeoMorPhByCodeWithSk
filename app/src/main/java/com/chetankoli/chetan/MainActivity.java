4f7adf190f37ec44a809417587aa0941ce16428a
chetankoli1 <mh18gamming@gmail.com>
2021-01-14 01:47:23 -0800

    first adding

---
+++ app/src/main/java/com/chetankoli/chetan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.chetankoli.chetan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.codewithsk.androidneomorphframelayout.NeomorphFrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
